f336db5e9217d11c5c881dec80a2fbce217979a3
Liberontissauri <jp.liberon@gmail.com>
2022-08-22 11:24:37 +0000

    added a room selection page

--- webapp/components/RoomList/index.js
+++ webapp/components/RoomList/index.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import styles from './styles.module.scss'
+import styles from './style.module.scss'
 
 function RoomList(props) {
   return (
     <div className={styles.container}>
-        
+        <p className={styles.header_name}>Name</p>
+        <p className={styles.header_protected}>Protected</p>
+        <p className={styles.header_players}>Players</p>
+        <div className={styles.room_item_container}>
+          {props.rooms.map(room => (
+            <div onClick={() => props.onClick(room)} className={styles.room_item} key={room.id}>
+              <p className={styles.room_name}>{room.name}</p>
+              <p className={styles.room_protected}>{room.protected ? 'Password' : 'Public'}</p>
+              <p className={styles.room_players}>{room.connected_players}/{room.max_players}</p>
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
--- webapp/pages/rooms/index.js
+++ webapp/pages/rooms/index.js
@@ -1,16 +1,93 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Button from '../../components/Button'
 import styles from '../../styles/Rooms.module.scss'
+import RoomList from '../../components/RoomList'
+import { useState } from 'react'
 
 export default function Rooms() {
+  const [roomList, setRoomList] = useState([{
+      id: 1,
+      name: "Room 1",
+      protected: false,
+      max_players: 4,
+      connected_players: 2,
+    },{
+      id: 1,
+      name: "Room 2",
+      protected: false,
+      max_players: 2,
+      connected_players: 1,
+    },
+    {
+      id: 1,
+      name: "Room 3",
+      protected: true,
+      max_players: 4,
+      connected_players: 3,
+    },
+    {
+      id: 1,
+      name: "Room 4",
+      protected: false,
+      max_players: 3,
+      connected_players: 1,
+    },
+    {
+      id: 1,
+      name: "Room 4",
+      protected: false,
+      max_players: 3,
+      connected_players: 1,
+    },
+    {
+      id: 1,
+      name: "Room 4",
+      protected: false,
+      max_players: 3,
+      connected_players: 1,
+    },
+    {
+      id: 1,
+      name: "Room 4",
+      protected: false,
+      max_players: 3,
+      connected_players: 1,
+    },
+    {
+      id: 1,
+      name: "Room 4",
+      protected: false,
+      max_players: 3,
+      connected_players: 1,
+    },
+    {
+      id: 1,
+      name: "Room 4",
+      protected: false,
+      max_players: 3,
+      connected_players: 1,
+    },
+    {
+      id: 1,
+      name: "Room 4",
+      protected: false,
+      max_players: 3,
+      connected_players: 1,
+    },
+
+  ])
+
   return (
     <div className={styles.container}>
       <div className={styles.bgElements}>
         <div className={styles.bgElementMagenta}></div>
         <div className={styles.bgElementBlue}></div>
       </div>
-      <h1>Word Bomb</h1>
+      <div className={styles.room_list_div}>
+        <div className={styles.room_list_header}>
+          <h3 className={styles.room_list_text}>Choose a room:</h3>
+        </div>
+        <RoomList onClick={(room)=>{}} rooms={roomList}></RoomList>
+      </div>
       <div className={styles.button_div}>
         <Button text="Create a Room" size="medium" />
       </div>
